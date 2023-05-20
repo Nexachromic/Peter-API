9cea410d333c4e3194abd72e36c2edd255396075
Dev-Atharv <invasionyt19@gmail.com>
2023-05-20 19:37:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,4 +79,4 @@ def roast(username):
     })
 
 if __name__ == '__main__':
-    app.run(host=""0.0.0.0", port=os.getenv("PORT", default=5000))
+    app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000))
